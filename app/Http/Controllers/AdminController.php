0738e1ef644601097f23d115ed7a895f12ea0968
Andrey Fidrya <findhostcoupons@gmail.com>
2025-04-15 13:50:04 +0300

    editing brand in admin panel

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,6 +6,7 @@ use App\Models\Brand;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\File;
 use Intervention\Image\Laravel\Facades\Image;
 
 class AdminController extends Controller
@@ -46,12 +47,46 @@ class AdminController extends Controller
 
     }
 
+    public function brand_edit($id)
+    {
+        $brand = Brand::find($id);
+        return view('admin.brand-edit', compact('brand'));
+    }
+
+    public function brand_update(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'slug' => 'required|unique:brands,slug,'.$request->id,
+            'image' => 'mimes:png,jpj,jpeg|max:2040'
+        ]);
+
+        $brand = Brand::find($request->id);
+        $brand->name = $request->name;
+        $brand->slug = Str::slug($request->name);
+        if($request->hasFile('image')){
+            if(File::exists(public_path('uploads/brands').'/'.$brand->image))
+            {
+                File::delete(public_path('uploads/brands').'/'.$brand->image);
+            }
+            $image = $request->file('image');
+            $file_extention = $request->file('image')->extension();
+            $file_name = Carbon::now()->timestamp.'.'.$file_extention;
+            $this->GenerateBrandThumbnailsImage($image,$file_name);
+            $brand->image = $file_name;
+        } 
+        
+        $brand->save();
+        return redirect()->route('admin.brands')->with('status','Brand has been updated succesfully!');
+
+    }
+
     public function GenerateBrandThumbnailsImage($image, $imageName)
     {
         $destinationPath = public_path('uploads/brands');
         $img = Image::read($image->path()); 
-        $img->cover(124,124,"top");
-        $img->resize(124,124,function($constraint){
+        $img->cover(200,200,"top");
+        $img->resize(200,200,function($constraint){
             $constraint->aspectRatio();
         })->save($destinationPath.'/'.$imageName);
     }
