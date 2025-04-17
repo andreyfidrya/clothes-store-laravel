45abd6948956c6b47ceea527333e81934d00aaf1
Andrey Fidrya <findhostcoupons@gmail.com>
2025-04-17 20:12:02 +0300

    Admin Product Page

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -4,7 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+
 class Brand extends Model
 {
     //
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,7 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+
 class Category extends Model
 {
     //
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
