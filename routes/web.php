c78fca65f2434395919074394ad2ec43cf6ceec1
Andrey Fidrya <findhostcoupons@gmail.com>
2025-05-01 22:27:37 +0300

    Admin Create Coupons Page + Admin Add New Coupon

--- routes/web.php
+++ routes/web.php
@@ -57,4 +57,9 @@ Route::middleware(['auth',AuthAdmin::class])->group(function(){
     Route::get('/admin/product/{id}/edit',[AdminController::class,'product_edit'])->name('admin.product.edit');
     Route::put('/admin/product/update',[AdminController::class,'product_update'])->name('admin.product.update');
     Route::delete('/admin/product/{id}/delete',[AdminController::class,'product_delete'])->name('admin.product.delete');
+
+    Route::get('/admin/coupons',[AdminController::class,'coupons'])->name('admin.coupons');
+    Route::get('/admin/coupon/add',[AdminController::class,'coupon_add'])->name('admin.coupon.add');
+    Route::post('/admin/coupon/store',[AdminController::class, 'coupon_store'])->name('admin.coupon.store');
+
 });
