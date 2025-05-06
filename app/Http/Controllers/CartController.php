639ee692ec7465e301ade226fee83512b8679ea5
Andrey Fidrya <findhostcoupons@gmail.com>
2025-05-06 17:03:41 +0300

    Removing Coupon from Cart

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -105,4 +105,12 @@ class CartController extends Controller
         
     }
 
+    public function remove_coupon_code()
+    {
+        Session::forget('coupon');
+        Session::forget('discounts');
+        return back()->with('success', 'Coupon has been removed!');
+    }
+
+
 }
