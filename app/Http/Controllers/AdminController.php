4dd4389c7ac36d9696cac5c66b9c5f576cdb5e58
Andrey Fidrya <findhostcoupons@gmail.com>
2025-05-21 21:03:10 +0300

    Admin Search Product

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -671,4 +671,11 @@ class AdminController extends Controller
         return redirect()->route('admin.contacts')->with("status", "Contact has been deleted successfully!");
     }
 
+    public function search(Request $request)
+    {
+        $query = $request->input('query');
+        $results = Product::where('name','LIKE',"%{$query}%")->get()->take(8);
+        return response()->json($results);
+    }
+
 }
