70a24747f9dee69668240c07a83e3418fdcc533d
Andrey Fidrya <findhostcoupons@gmail.com>
2025-06-24 19:43:52 +0300

    shortening input fields

--- resources/views/user/account-details.blade.php
+++ resources/views/user/account-details.blade.php
@@ -21,35 +21,35 @@
                 @csrf
                 @method('PUT')
                 <div class="row">
-                  <div class="col-md-6">
+                  <div class="col-md-7">
                     <div class="form-floating my-3">
                       <input type="text" class="form-control" placeholder="Full Name" name="name" value="{{$user->name}}" required="">
                       <label for="name">Name</label>
                     </div>
                   </div>
                   @error('name') <span class="alert alert-danger text-center">{{$message}} </span> @enderror
-                  <div class="col-md-12">
+                  <div class="col-md-7">
                     <div class="form-floating my-3">
                       <input type="text" class="form-control" placeholder="Mobile Number" name="mobile" value="{{$user->mobile}}" required="">
                       <label for="mobile">Mobile Number</label>
                     </div>
                   </div>
                   @error('mobile') <span class="alert alert-danger text-center">{{$message}} </span> @enderror
-                  <div class="col-md-12">
+                  <div class="col-md-7">
                     <div class="form-floating my-3">
                       <input type="email" class="form-control" placeholder="Email Address" name="email" value="{{$user->email}}" required="">
                       <label for="account_email">Email Address</label>
                     </div>
                    </div>
                   @error('email') <span class="alert alert-danger text-center">{{$message}} </span> @enderror
-                   <div class="col-md-12">
+                   <div class="col-md-7">
                     <div class="form-floating my-3">
                       <input type="password" class="form-control" placeholder="Current Password" name="current_password" value="" required="">
                       <label for="account_email">Current Password</label>
                     </div>
                    </div>
                   @error('current_password') <span class="alert alert-danger text-center">{{$message}} </span> @enderror
-                   <div class="col-md-12">
+                   <div class="col-md-7">
                         <div class="my-3">
                         <button type="submit" class="btn btn-primary">Save Changes</button>
                         </div>
